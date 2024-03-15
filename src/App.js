59d37e1dabf8ac7b37d43c0cf7a8f9613e68283a
Darko Crnic <darko.crnic@qcerris.com>
2024-03-15 17:08:19 +0100

    add MUI icons and try out SvgIcon wrapper component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React from "react";
 import Button from "@mui/material/Button";
-import Fav from "./assets/icons/favorite.svg";
+import fav from "./assets/icons/favorite.svg";
+import zoomIcon from "./assets/icons/zoom-in.svg";
+import SvgIcon from "@mui/material/SvgIcon";
 
 const App = () => {
+  console.log(zoomIcon);
+  const ImageWithZoomIcon = () => {
+    return (
+      <div style={{ position: "relative", display: "inline-block" }}>
+        {/* <img src={Fav} alt="Your Image" style={{ width: "100%" }} /> */}
+        <SvgIcon
+          component={fav}
+          inheritViewBox
+          alt="Zoom Icon"
+          style={{ fontSize: "500px" }}
+        />
+        <div style={{ position: "absolute", bottom: 0, right: 0 }}>
+          <SvgIcon
+            component={zoomIcon}
+            inheritViewBox
+            alt="Zoom Icon"
+            style={{ width: "30px", height: "30px" }}
+          />
+          {/* <img src={zoomIcon} /> */}
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div>
-      Testing heroku deploy <Button>Test Button</Button>
-      <img src={Fav}></img>
+      <ImageWithZoomIcon />
+      <div>
+        Testing heroku deploy <Button>Test Button</Button>
+      </div>
     </div>
   );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -27,14 +27,7 @@ module.exports = {
       },
       {
         test: /\.svg$/,
-        use: [
-          {
-            loader: "svg-url-loader",
-            options: {
-              limit: 10000,
-            },
-          },
-        ],
+        use: ["@svgr/webpack"],
       },
     ],
   },
