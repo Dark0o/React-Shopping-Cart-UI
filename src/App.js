1c635bdd17e54fb30e74b56327857c0d5819e476
Darko Crnic <darko.crnic@qcerris.com>
2024-03-15 14:35:29 +0100

    intall and test MUI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Button from "@mui/material/Button";
 
 const App = () => {
-  return <div>Testing heroku deploy</div>;
+  return (
+    <div>
+      Testing heroku deploy <Button>Test Button</Button>
+    </div>
+  );
 };
 
 export default App;
